647e15a9afa5a670afc254669e7e331c36da047b
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-06-07 19:56:39 -0700

    Time: 2 ms (97.99%) | Memory: 44.7 MB (34.58%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        s = new String(sArray);
+        t = new String(tArray);
+
+        return s.equals(t);
+        
+    }
+}
