76bb7bc8795108aec99934144961248093fc02f4
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-06-12 16:46:02 -0700

    Time: 1 ms (98.90%) | Memory: 44.6 MB (95.64%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,56 +1,34 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-
+        if(intervals == null || intervals.length <1 ) return new int[][]{newInterval};
         List<int[]> ret = new ArrayList<>();
 
-        if(intervals == null || intervals.length<1){
-            ret.add(newInterval);
-            return ret.toArray(new int[0][]);
+        // we'll divide the problem into three parts,
+
+
+ 
+
+        int newStart = newInterval[0], newEnd = newInterval[1];
+        int i=0;
+        // first well add all the intervals that are before the beginning of newInterval
+        while(i <intervals.length && intervals[i][1] < newStart){
+            ret.add(intervals[i]);
+            i++;
         }
-        int i;
-        int[] last ={};
-
-        for(i=0;i<intervals.length;i++){
-            int[] curr = intervals[i];
-            if(curr[1] >= newInterval[0]){
-                if(newInterval[1] < curr[0]){
-                    last = newInterval;
-                }else{
-                curr[1] = Math.max(curr[1],newInterval[1]);
-                curr[0] = Math.min(curr[0], newInterval[0]);
-                last = curr;
-                i++;
-                if(i == intervals.length){
-                    ret.add(curr);
-                    return ret.toArray(new int[0][]);
-                }
-                }
-                break;
-            }else{
-                ret.add(curr);
-                if(i == intervals.length-1){
-                    ret.add(newInterval);
-                    return ret.toArray(new int[0][]);
-                }
-            }
+        // second we'll add all the intervals that are merging with the new interval 
+        while(i <intervals.length && newEnd >= intervals[i][0]){
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
         }
+        ret.add(newInterval);
+       // last we'll add the remaining ones. 
+       while(i < intervals.length){
+        ret.add(intervals[i]);
+        i++;
+       }
 
-        for(int j=i;j<intervals.length;j++){
-            int[] curr = intervals[j];
+       return ret.toArray(new int[0][]);
 
-            if(last[1] >= curr[0]){
-                last[0] = Math.min(curr[0], last[0]);
-                last[1] = Math.max(curr[1], last[1]);
-            }else{
-                ret.add(last);
-                last = curr;
-            }
-        }
-
-        ret.add(last);
-
-        return ret.toArray(new int[0][]);
-
-        
     }
 }
