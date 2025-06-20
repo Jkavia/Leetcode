434c6250ac2efbea9bee6f143a2607ba1828c526
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-06-19 21:50:15 -0700

    Time: 0 ms (100.00%) | Memory: 45.5 MB (91.85%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,23 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> rowWithZero = new HashSet<>();
-        Set<Integer> colWithZero = new HashSet<>();
+        boolean firstZero = false;
         
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
+            if(matrix[i][0] == 0) firstZero = true;
+            for(int j=1;j<matrix[0].length;j++){
                 if(matrix[i][j] == 0){
-                    rowWithZero.add(i);
-                    colWithZero.add(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(rowWithZero.contains(i) || colWithZero.contains(j)){
+        for(int i=matrix.length-1;i>=0;i--){
+            for(int j=matrix[0].length-1;j>0;j--){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] =0;
                 }
             }
+            if(firstZero) matrix[i][0] = 0;
         }
 
     }
