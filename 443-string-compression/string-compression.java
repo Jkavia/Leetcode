dd1a742d8f6876cbb28665ad654cb04e5ce83ec0
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-06-23 17:39:58 -0700

    Time: 1 ms (99.55%) | Memory: 44.1 MB (54.14%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,31 +1,31 @@
 class Solution {
     public int compress(char[] chars) {
-        int count =1;
-        StringBuffer sb = new StringBuffer();
+        // We can run the loop on char array>
+        // 1. pick one character and from current i start comparing all the elements from that char 
+        // 2. keep incrementing i and count as long as there is match 
+        // 3. Once there is a mismatch set this character at jth index(starting 0) in char array and do j++
+        // 4. To add count if its 1 skip it, if more than make the int into chararray and add it to the chars.
+        // 5. In next iteration it will again repeat the same process. 
+        if(chars.length == 1) return 1;
+        int count =0;
+        int idx =0;
+        for(int i=0;i<chars.length;){
+            char curr = chars[i];
 
-        for(int i=1;i<chars.length;i++){
-            if(chars[i] == chars[i-1]){
+            while(i<chars.length && curr == chars[i]){
                 count++;
-            }else{
-                sb.append(chars[i-1]);
-                if(count > 1){
-                  sb.append(count);
-                  count =1;
+                i++;
+            }
+
+            chars[idx++] = curr;
+            if(count > 1){
+                for(char c: String.valueOf(count).toCharArray()){
+                    chars[idx++] = c;
                 }
             }
+            count =0;
         }
 
-        sb.append(chars[chars.length-1]);
-        if(count > 1){
-            sb.append(count);
-            }
-            int index =0;
-
-        for(char c: sb.toString().toCharArray()){
-            chars[index++] = c;
-        }
-        return sb.length();
+        return idx;
     }
 }
-//["a","a","b","b","c","c","c"]
-// a 2 b 2 3
