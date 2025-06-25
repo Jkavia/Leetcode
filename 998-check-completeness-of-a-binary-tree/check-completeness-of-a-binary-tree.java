1d9e386127dcc5569996dde6f28ca6bf1af06cdc
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-06-24 19:26:43 -0700

    Time: 1 ms (85.40%) | Memory: 42.1 MB (72.26%) - LeetSync

---
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root);
+        boolean firstNullFound = false;
+        boolean isLastLevel = false;
+        while (!que.isEmpty()) {
+            int size = que.size();
+            int nonnullcount =0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = que.poll();
+                if (node == null) {
+                    firstNullFound = true;
+                    isLastLevel = true;
+                    continue;
+                }
+                if (firstNullFound && node != null) {
+                    return false;
+                }
+                if(node.left !=null){
+                    nonnullcount++;
+                }
+                
+                if(node.right !=null){
+                    nonnullcount++;
+                }
+                que.add(node.left);
+                que.add(node.right);
+            }
+            //that means there was null on second last level
+            if(isLastLevel && nonnullcount>0)return false;
+            firstNullFound = false;
+        }
+
+        return true;
+    }
+}
