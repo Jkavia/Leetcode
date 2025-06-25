3d7663b07130221a307fca40417a48a4fa1b9327
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-06-24 21:28:26 -0700

    Time: 1 ms (100.00%) | Memory: 44 MB (93.50%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,48 +1,24 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int limCap = capacity;
-        
-        PriorityQueue<int[]> earliestDestination = new PriorityQueue<>((a,b) -> Integer.compare(a[0],b[0]));
-        // keep track of prevStart so that next start can not be lesser i.e. in west.
-        int prevStart = Integer.MIN_VALUE;
-        Arrays.sort(trips, (a,b) -> Integer.compare(a[1],b[1]));
-        for(int[] trip: trips){
-            int earliestDest = 
-            earliestDestination.isEmpty()? Integer.MAX_VALUE: earliestDestination.peek()[0];
-            
-            //east to west move next starting is before the start of prev. 
-            if(trip[1] < prevStart)return false;
-            if(trip[1] < earliestDest){
-                if(capacity - trip[0] <0){
-                    return false;
-                }else{
-                    capacity -= trip[0];
-                    earliestDestination.add(new int[]{trip[2], trip[0]});
-                }
-            }else{
-                //Adjust capacity first then check if current trip can be made
-                while(!earliestDestination.isEmpty()){
-                    if(earliestDestination.peek()[0] <= trip[1]){
-                        int[] val = earliestDestination.poll();
-                        
-                        capacity = capacity + val[1];
-                        System.out.println(" val0 "+val[0]+" val1"+val[1]+" cap "+capacity);
-                        if(capacity > limCap){
-                            capacity = limCap;
-                        }
-                    }else{
-                        break;
-                    }
-                }
+        //since there are only 1000 stops we just need to track how many people 
+        // got in/out of the car and then we can just iterate through the stops 
+        //adjusting our capacity and comparing it to overall, if it goes above the limit we return false
 
-                if(capacity - trip[0] <0) return false;
-                capacity -= trip[0];
-                earliestDestination.add(new int[]{trip[2], trip[0]});
-            }
-            prevStart = trip[1] ;
+        int[] destinations = new int[1001];
+        for(int[] trip:trips){
+            int passenger = trip[0];
+            int st = trip[1];
+            int end = trip[2];
+            destinations[st] += passenger;
+            destinations[end] -= passenger;
         }
 
-        return true;
+        int currCapacity =0;
+        for(int i =0;i<destinations.length;i++){
+            currCapacity += destinations[i];
 
+            if(currCapacity > capacity)return false;
+        }
+        return true;
     }
 }
