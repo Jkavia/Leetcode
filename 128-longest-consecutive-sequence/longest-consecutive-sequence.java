5d04d597b775746bacb3c15838d1c868e7746e3e
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-06-25 23:02:45 -0700

    Time: 92 ms (5.04%) | Memory: 62.3 MB (64.79%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,23 +1,27 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums == null || nums.length == 0)return 0;
-        Arrays.sort(nums);
-        int currSeq =1;
-        int longestseq =0;
-        int prev = nums[0];
-        
+        if(nums == null || nums.length < 1) return 0;
+        TreeSet<Integer> minHeap = new TreeSet<>();
 
-        for(int i=1;i<nums.length;i++){
-            if(nums[i] == prev+1){
-                currSeq++;
-            }else if(nums[i] == prev){
-                //do nothing
-            }else{
-                longestseq = Math.max(currSeq, longestseq);
-                currSeq =1;
-            }
-            prev = nums[i];
+        for(int num:nums){
+            minHeap.add(num);
         }
-        return Math.max(currSeq, longestseq);
+        int maxLen =1;
+        int prev = minHeap.pollFirst();
+        int tempLen =1;
+
+        while(!minHeap.isEmpty()){
+            int curr = minHeap.pollFirst();
+
+            if(prev+1 == curr){
+                tempLen++;
+            }else{
+                maxLen = Math.max(maxLen, tempLen);
+                tempLen = 1;
+            }
+            prev = curr;
+        }
+
+        return Math.max(maxLen, tempLen);
     }
 }
