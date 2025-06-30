f1f83b19bc87e7af7b2eafcb21d5f55d0374051e
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-06-29 23:06:47 -0700

    Time: 1 ms (99.49%) | Memory: 44 MB (79.73%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,16 +1,13 @@
 class Solution {
     public int compress(char[] chars) {
-        // We can run the loop on char array>
-        // 1. pick one character and from current i start comparing all the elements from that char 
-        // 2. keep incrementing i and count as long as there is match 
-        // 3. Once there is a mismatch set this character at jth index(starting 0) in char array and do j++
-        // 4. To add count if its 1 skip it, if more than make the int into chararray and add it to the chars.
-        // 5. In next iteration it will again repeat the same process. 
-        if(chars.length == 1) return 1;
-        int count =0;
+        // use two ptrs one is moving counting letters and other is moving counting updated representation with numbers
         int idx =0;
+        int count =0;
+
         for(int i=0;i<chars.length;){
             char curr = chars[i];
+            count =1;
+            i++;
 
             while(i<chars.length && curr == chars[i]){
                 count++;
@@ -18,14 +15,14 @@ class Solution {
             }
 
             chars[idx++] = curr;
+
             if(count > 1){
-                for(char c: String.valueOf(count).toCharArray()){
+                char[] arr = String.valueOf(count).toCharArray();
+                for(char c: arr){
                     chars[idx++] = c;
                 }
             }
-            count =0;
         }
-
-        return idx;
+        return idx;  
     }
 }
