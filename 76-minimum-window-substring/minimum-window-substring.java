62dba28ed1a48d1da6adea3ad227ebe69e29d89b
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-06-30 14:49:43 -0700

    Time: 17 ms (62.79%) | Memory: 44.8 MB (80.70%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,40 +1,45 @@
 class Solution {
     public String minWindow(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
 
-        Map<Character, Integer> charMap = new HashMap<>();
-        for(char c:t.toCharArray()){
-            charMap.put(c, charMap.getOrDefault(c,0)+1);
-        }
-        int count = charMap.size();
-        int start = 0, minlen = Integer.MAX_VALUE;
-        int p1=0,p2=0, slen = s.length();
-
-        while(p2 < slen){
-            char cr = s.charAt(p2);
-            if(charMap.containsKey(cr)){
-                charMap.put(cr, charMap.get(cr)-1);
-                if(charMap.get(cr)==0)count--;
-            }
-            p2++;
-            while(count ==0 && p1<=p2){
-                    if(p2-p1 < minlen){
-                        start = p1;
-                        minlen = p2-p1;
-                    }
-                    char temp = s.charAt(p1);
-                    p1++;
-                    if(charMap.containsKey(temp)){
-                        charMap.put(temp, charMap.get(temp)+1);
-                        if(charMap.get(temp)>0)count++;
-
-                    }
-            }
+        for(char c: t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
         }
 
-         if(minlen == Integer.MAX_VALUE)return "";
+        int size = map.size();
+        int count =0;
+        int[] coordinates = new int[]{0, s.length()-1};
+        int minLen = Integer.MAX_VALUE;
+        int start =0, end =0;
 
-        return s.substring(start, start+minlen);
-        
+        while(end < s.length()){
+            char c = s.charAt(end);
+            if(map.containsKey(c)){
+                map.put(c, map.get(c)-1);
+                //System.out.println("before: "+c+"   "+map.get(c));
+                if(map.get(c) == 0)count++;
+            }
+            end++;
+            while(start < s.length() && count == size){
+                //System.out.println(start+"   "+end);
+                if(minLen > end-start){
+                    minLen = end-start;
+                    coordinates[0] = start;
+                    coordinates[1] = end;
+                }
+                char sChar = s.charAt(start);
+                if(map.containsKey(sChar)){
+                map.put(sChar, map.get(sChar)+1);
+                //System.out.println("afterUp: "+sChar+"   "+map.get(sChar));
+                if(map.get(sChar) > 0){
+                    //System.out.println("after: "+sChar+"   "+map.get(sChar));
+                    count--;}
+                }
+                start++;
+            }   
+        }
+        if(minLen == Integer.MAX_VALUE)return "";
+        return s.substring(coordinates[0],coordinates[1]);
+        }
         
     }
-}
