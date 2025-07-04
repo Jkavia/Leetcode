190d2b48e861c7077b9c6d054b1e83b9c41de903
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-04 00:36:48 -0700

    Time: 33 ms (5.23%) | Memory: 45.5 MB (15.86%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,39 @@
+//we'll do a binary search from 1 to 1000000000 (as its the max value)
+// at each mid point we'll check if the hours taken are more or less that available
+// based on that info we'll move our left and right pointers. 
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left =1;
+        int right = 1000000000;// defined max possible value, if not we can do a loop to get max 
+
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            System.out.println("mid:" +mid);
+
+            if(canEatInTime(mid, piles, h)){
+                //need to reduce the speed
+                right = mid-1;
+            }else{
+                //need to increase the speed.
+                left = mid+1;
+            }
+        }
+
+        return left;
+    }
+
+    public boolean canEatInTime(int mid, int[] piles, int h){
+        long hours =0;
+
+        for(int pile: piles){
+            int div = pile/mid;
+            hours = hours+div;
+            if(pile%mid != 0)hours++;
+        }
+        //System.out.println("hours:" +hours);
+        return (hours>=0 && hours <= h);
+    }
+
+
+}
