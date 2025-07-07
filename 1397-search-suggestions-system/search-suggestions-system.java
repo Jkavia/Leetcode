37379748cc6844a27fa953d56b1b6e6658fb7f05
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-06 18:13:38 -0700

    Time: 28 ms (46.81%) | Memory: 47.4 MB (69.76%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.java
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -1,22 +1,26 @@
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        
-        //brute force method just pick first letter of search word look up
-        // then pick first two letters look up and so on. 
+        // Other way is to skip sorting the array and instead use a PQ or treeSet and 
+        // fix its length to no more than 3, that way it will auto sort as we add
+        // and sorting complexity will be log(3) so constant. 
 
         List<List<String>> retlist = new ArrayList<>();
-        Arrays.sort(products);
+        TreeSet<String> set = new TreeSet<>();
+
         for(int i=1;i<=searchWord.length();i++){
             String currWord = searchWord.substring(0,i);
-            List<String> currLayerList = new ArrayList<>();
+
             for(String product:products){
                 if(product.startsWith(currWord)){
-                    currLayerList.add(product);
+                    set.add(product);
+                    if(set.size()>3)set.pollLast();
                 }
-                if(currLayerList.size() == 3)break;
             }
-                retlist.add(currLayerList);
+            List<String> currList = new ArrayList<>(set);
+            retlist.add(currList);
+            set.clear();
         }
+
         return retlist;
     }
 }
