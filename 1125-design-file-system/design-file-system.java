b9e714c565f19d6da4d7fcdc46354bf646d0d577
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-09 14:36:01 -0700

    Time: 81 ms (77.54%) | Memory: 55.5 MB (81.87%) - LeetSync

--- 1125-design-file-system/design-file-system.java
+++ 1125-design-file-system/design-file-system.java
@@ -1,34 +1,31 @@
+// we will create a map of paths and values, to check for parent we will see if the substring before last '/' exist ? if
+// it doesnt then we skip it. 
+// if path alredy exists return false;
 class FileSystem {
-    // Other way to do this is, store all paths on the same map, 
-    // and check the parent existence by checking if the path before last "/" already exists in map. 
-    Map<String,Integer> paths;
+    Map<String, Integer> paths;
+
+    
 
     public FileSystem() {
         paths = new HashMap<>();
     }
     
     public boolean createPath(String path, int value) {
-        // lets do some basic checks here and prevent overwriting 
-        if(path.isEmpty() || path==null || path.indexOf("/") == -1 || (path.length() == 1 && path.equals("/")) || paths.containsKey(path)){
-                  
-            return false;
+        if(path.isEmpty() || path == null || (path.length() == 1 && path.equals("/")) || paths.containsKey(path))return false;
+
+        int lastIdx = path.lastIndexOf("/");
+
+        if(lastIdx > 0){
+            String parent = path.substring(0,lastIdx);
+            if(!paths.containsKey(parent))return false;
         }
 
-        int indexOfLastSlash = path.lastIndexOf("/");
-        if(indexOfLastSlash > 0){
-        String parent = path.substring(0,indexOfLastSlash);
-        // parent doent exist
-        if(!paths.containsKey(parent)){
-            return false;
-        }
-        }
-
-        paths.put(path, value);
+        paths.put(path,value);
         return true;
     }
     
     public int get(String path) {
-        return paths.getOrDefault(path,-1);
+        return paths.getOrDefault(path, -1);
     }
 }
 
