7947dd98cf9f24eb862142e336e190cef863e37a
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-09 15:50:31 -0700

    Time: 41 ms (15.28%) | Memory: 45.7 MB (86.01%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,25 +1,31 @@
+// we will use priority queue and store the distances 
+// then we'll poll the queue to get the top 4
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        //we will try to do a binary search to find out the range 
-        // where we can get the K closest elements 
-
-        int left =0, right = arr.length - k;
-        // we subtracted K from right to accomodate for the calculations if range comes out to be last k elements 
-
-        while(left < right){
-            int mid = left + (right-right)/2;
-
-            if(x- arr[mid] > arr[mid+k] - x){
-                left = mid+1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> {
+            if(Math.abs(a-x) == Math.abs(b-x)){
+                return Integer.compare(a,b);
             }else{
-                right = mid;
+                int num1 = Math.abs(a-x);
+                int num2 = Math.abs(b-x);
+                return Integer.compare(num1, num2);
             }
+        });
+
+        for(int a: arr){
+            pq.add(a);
         }
         List<Integer> ret = new ArrayList<>();
-        for(int i=left;i<left+k;i++){
-            ret.add(arr[i]);
+
+        while(ret.size()<k){
+            ret.add(pq.poll());
         }
+
+        Collections.sort(ret);
 
         return ret;
     }
 }
+
+// 1 2 4 5 6
+// 4 3 2 0 1
