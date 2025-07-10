c9ebdba667dae11f1491b8533e0aad0200e575ad
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-10 13:14:42 -0700

    Time: 8 ms (78.13%) | Memory: 48 MB (63.87%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.java
+++ 286-walls-and-gates/walls-and-gates.java
@@ -1,39 +1,42 @@
 class Solution {
-    // we'll start from breadth first search at gates
-    // and we'll proceed with incrementing counter and updating the cell values with that counter. 
     public void wallsAndGates(int[][] rooms) {
-        if(rooms == null || rooms.length ==0)return;
-
-        Queue<int[]> que = new LinkedList<>();
-
+        int maxVal = Integer.MAX_VALUE;
+        int distance = 1;
         int row = rooms.length;
         int col = rooms[0].length;
 
-        for(int i =0;i<row;i++){
+        Queue<int[]> que = new LinkedList<>();
+        for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(rooms[i][j] == 0) que.offer(new int[]{i,j});
+                // if its a gate add it to the que.
+                if(rooms[i][j] == 0){
+                    que.add(new int[]{i,j});
+                }
             }
         }
-        int count =0;
+
         while(!que.isEmpty()){
-            count++;
             int size = que.size();
-            for(int x=0;x<size;x++){
+            int[][] directions = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+
+            for(int a=0;a<size;a++){
                 int[] cell = que.poll();
 
-                int[][] dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+                for(int[] dir:directions){
+                    int x = cell[0]+dir[0];
+                    int y = cell[1]+dir[1];
 
-                for(int[] dir: dirs){
-                    int nrow = dir[0]+cell[0];
-                    int ncol = dir[1]+cell[1];
-
-                    if(nrow>= 0 && nrow<rooms.length && ncol>= 0 && ncol<rooms[0].length && rooms[nrow][ncol] == Integer.MAX_VALUE){
-                        rooms[nrow][ncol] =  count;
-                        que.offer(new int[]{nrow,ncol});
+                    if(x>=0 && x<row && y>=0 && y<col && rooms[x][y]!=0 && rooms[x][y]!=-1 && rooms[x][y]==maxVal){
+                        rooms[x][y] = distance;
+                        que.add(new int[]{x,y});
                     }
                 }
-
             }
+            distance++;
         }
     }
 }
+
+// we'll do a bfs from gates 
+// to track the visited attribute we'll just perfrom a check for maxValue present if not update it
+// we dont need to do a min on the cells since if a cell is already visited that means it was closer to the older gate than current one.
