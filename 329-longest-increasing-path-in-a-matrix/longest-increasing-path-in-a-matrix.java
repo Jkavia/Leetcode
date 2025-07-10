064bc86eddf5bf966ad71e72a9650c47f8603915
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-10 14:21:00 -0700

    Time: 7 ms (96.93%) | Memory: 45.4 MB (32.80%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,56 +1,40 @@
 class Solution {
+    int longestPath = Integer.MIN_VALUE;
     public int longestIncreasingPath(int[][] matrix) {
-        //so it seems like this is a problem of DP 
-        // because if you parse one path from index i,j then you also end up finding the longest path for its
-        // children too, and we dont want to repeat that process again and reuse that longest path 
-        // value of its child in future calculations 
-        // for that we will create another matrix with -1 in all boxes and do a recursive call until we break the 
-        // rule of either reaching boundary or there is no more greater values nearby.
-        // now one intersting thing is there might be two or four possible paths from each point and in that case we
-        // want to save the maximum path value from that point, so run a for loop on all sides and save the max. 
         int row = matrix.length;
         int col = matrix[0].length;
-        int longestSum = Integer.MIN_VALUE;
+        int[][] directions = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
         int[][] dp = new int[row][col];
+
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(dp[i][j]==0){
-                int pathSum = longestPathSumForBox(matrix, dp, new int[]{i, j});
-                longestSum = Math.max(longestSum, pathSum);
+                    dfs(i,j,dp,matrix,directions);
                 }
             }
         }
 
-        return longestSum;  
+        return longestPath;
     }
 
 
-    public int longestPathSumForBox(int[][] matrix, int[][] dp, int[] node){
+    public int dfs(int row, int col, int[][] dp, int[][] matrix,int[][] directions){
+        if(dp[row][col] != 0)return dp[row][col];
 
-        int r = node[0], c = node[1];
+        int maxSumSide = 0;
 
-        if(dp[r][c] != 0)return dp[r][c];
+        for(int[] dir:directions){
+            int x = row+dir[0];
+            int y = col+dir[1];
 
-        int[][] dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-
-        int maxPathNode = 1;
-        //visited.add(node);
-        for(int[] dir: dirs){
-            int val = matrix[r][c];
-            int nRow = r+dir[0];
-            int nCol = c+dir[1];
-            if(nRow>=0 && nRow<matrix.length && nCol>=0 && nCol<matrix[0].length && matrix[nRow][nCol]>val){
-                node[0] = nRow;
-                node[1] = nCol;
-                int currPath = longestPathSumForBox(matrix, dp, node)+1;
-                maxPathNode = Math.max(maxPathNode, currPath);
+            if(x>=0 && x<matrix.length && y>=0 && y<matrix[0].length && matrix[row][col]<matrix[x][y]){
+                int dist = dfs(x,y,dp,matrix,directions);
+                maxSumSide = Math.max(maxSumSide, dist);
             }
         }
-        dp[r][c] = maxPathNode;
-        //visited.remove(node);
 
-        return maxPathNode;
+        dp[row][col] = maxSumSide+1;
+        longestPath = Math.max(longestPath, dp[row][col]);
+        return dp[row][col];
     }
-
-
 }
