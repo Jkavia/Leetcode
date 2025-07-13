c81b223829ec9c8285367d0e0d73885a7478a5c8
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-12 18:51:53 -0700

    Time: 3 ms (39.20%) | Memory: 42.3 MB (8.32%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,27 +1,35 @@
 class Solution {
     public boolean isValid(String s) {
-        Map<Character, Character> charMap = new HashMap<>();
-        charMap.put(')','(');
-        charMap.put('}','{');
-        charMap.put(']','[');
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> openingStyle = new HashSet<>();
+        Stack<Character> stack = new Stack<>();
 
-        Stack<Character> stk = new Stack<>();
+        map.put('}', '{');
+        map.put(']', '[');
+        map.put(')', '(');
+        openingStyle.addAll(Arrays.asList('(','[','{'));
 
-        for (char c: s.toCharArray()){
-
-            if(charMap.containsKey(c)){
-
-                if(stk.isEmpty() || charMap.get(c)!= stk.peek()){
+        for(char c:s.toCharArray()){
+            if(openingStyle.contains(c)){
+                stack.push(c);
+            }else{
+                char closingchar = map.get(c);
+                if(!stack.isEmpty() && stack.peek() == closingchar){
+                    stack.pop();
+                }else{
                     return false;
                 }
-                stk.pop();
-
-            }else{
-                stk.push(c);
             }
         }
-        return stk.isEmpty();
+
+        return stack.isEmpty();
 
         
     }
 }
+
+// use map to track the closing bracket char for each opening type. 
+// use stack to store the opening brackets. 
+// and whenever you see a closing bracket you should check if the bracket against it in map is 
+// the top element in stack, if true pop stack element 
+// if stack is null or there is a mismatch return false. 
