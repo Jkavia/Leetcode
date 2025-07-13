e2ac0516490965bb9daf3825970240b458065e68
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-12 19:16:08 -0700

    Time: 9 ms (49.16%) | Memory: 44.7 MB (89.25%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,16 +1,31 @@
 class Solution {
+
     public int[] twoSum(int[] nums, int target) {
-        
-        int p1 = 0, p2=nums.length-1;
-        Map<Integer, Integer> imap = new HashMap<>();
+        List<int[]> list = new ArrayList<>();
 
         for(int i=0;i<nums.length;i++){
-            if(imap.containsKey(target-nums[i])){
-                return new int[]{imap.get(target-nums[i]),i};
+            list.add(new int[]{nums[i],i});
+        }
+
+        Collections.sort(list, (a,b) -> Integer.compare(a[0],b[0]));
+
+        int l=0,r=list.size()-1;
+
+        while(l<r){
+            int num = list.get(l)[0]+list.get(r)[0];
+            if(num == target){
+                return new int[]{list.get(l)[1], list.get(r)[1]};
+            }else if(num > target){
+                r--;
+            }else{
+                l++;
             }
-            imap.put(nums[i],i);
         }
 
         return new int[2];
     }
 }
+
+// just parse through the array and look for target-curr in map and if its duplicate numbers also check 
+// count > 1
+
