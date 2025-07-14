2bfd7cae8e78df8a67f5de1a63886a01b678b3ed
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-14 12:21:57 -0700

    Time: 6 ms (87.02%) | Memory: 45.1 MB (23.51%) - LeetSync

--- 723-candy-crush/candy-crush.java
+++ 723-candy-crush/candy-crush.java
@@ -1,25 +1,20 @@
-//this problem has two parts 
-// one to check the possible crush zones and flag them
-// second to gravitiate crush those zones by moving up two pointers for each column 
-// one final part is to know when to stop, to mark that we'll use a flag that will tell us if we
-// found any possible crush zones in current iteration or not. 
 class Solution {
     public int[][] candyCrush(int[][] board) {
         int row = board.length;
         int col = board[0].length;
-        boolean needToGravivate = true;
+        boolean repeat = true;
 
-        while (needToGravivate) {
-            needToGravivate = false;
-
-            for (int i = 0; i < row; i++) {
-                for (int j = 0; j < col; j++) {
-                    if (board[i][j] != 0 && checkCrushZones(i, j, board)) {
-                        needToGravivate = true;
+        while(repeat){
+            repeat = false;
+            for(int i=0;i<row;i++){
+                for(int j=0;j<col;j++){
+                    if(board[i][j]!= 0 && isCrushable(i,j,board)){
+                        repeat = true;
                     }
                 }
             }
-            if (needToGravivate) {
+
+            if(repeat){
                 gravitate(board);
             }
         }
@@ -31,51 +26,51 @@ class Solution {
     public void gravitate(int[][] board){
         int row = board.length;
         int col = board[0].length;
-        // we'll do the traversal in colum by column manner. last row to first row for each column
 
-        for(int j=0;j<col;j++){
-            //use two pointers to track negative and non negative for replacements.
-            // p1 only moves when cell is positive 
-            int p1 = row-1;
-            for(int p2 = row-1;p2>=0;p2--){
-                if(board[p2][j]>0){
-                    board[p1][j] = board[p2][j];
-                    p1--;
+        for(int c=0;c<col;c++){
+            int rp1 = row-1;
+            for(int rp2=row-1;rp2>=0;rp2--){
+                if(board[rp2][c]>0){
+                    board[rp1][c] = board[rp2][c];
+                    rp1--;
                 }
             }
 
-            //once we have reached the top of the column for p2, then whatever values remain for p1 to travel, we;ll mark them 0
-            for(int i=p1;i>=0;i--){
-                board[i][j] = 0;
+            // set remaining to 0
+            for(int i=rp1;i>=0;i--){
+                board[i][c] =0;
             }
         }
     }
 
-    public boolean checkCrushZones(int i, int j, int[][] board) {
+
+    public boolean isCrushable(int i,int j, int[][] board){
         int row = board.length;
         int col = board[0].length;
         int val = Math.abs(board[i][j]);
         boolean ret = false;
 
-        if (i + 2 <= row - 1) {
-            if ((Math.abs(board[i + 1][j]) == val) && (Math.abs(board[i + 2][j]) == val)) {
-                ret = true;
-                board[i][j] = val * -1;
-                board[i + 1][j] = val * -1;
-                board[i + 2][j] = val * -1;
-            }
+        if(i+2 <= row-1 && Math.abs(board[i+1][j])==val && Math.abs(board[i+2][j])==val){
+            board[i][j] = val*-1;
+            board[i+1][j] = val*-1;
+            board[i+2][j] = val*-1;
+            ret = true;
         }
 
-        if (j + 2 <= col - 1) {
-            if ((Math.abs(board[i][j+1]) == val) && (Math.abs(board[i][j+2]) == val)) {
-                ret = true;
-                board[i][j] = val * -1;
-                board[i][j+1] = val * -1;
-                board[i][j+2] = val * -1;
-            }
+        if(j+2 <= col-1 && Math.abs(board[i][j+1])==val && Math.abs(board[i][j+2])==val){
+            board[i][j] = val*-1;
+            board[i][j+1] = val*-1;
+            board[i][j+2] = val*-1;
+            ret = true;
         }
 
         return ret;
-
     }
 }
+
+// we'll solve this problem in two parts 
+// first is to find the cruhsable zones and we'll make them -ve of current value (2 down, 2 right compare)
+// next we'll check if there were any crushable zones then we'll call gravitate functions to move the cells 
+// in column by column manner. 
+// to run gravitate we'll use use two pointer approach where one pointer will keep moving and other will only move 
+// if the position in positive number. 
