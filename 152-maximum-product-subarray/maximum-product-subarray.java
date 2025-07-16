dbc7d8d6f93126f5a3ff7abd96acbd42dfc02721
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-15 17:42:35 -0700

    Time: 2 ms (55.13%) | Memory: 45 MB (43.96%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+        int currMax = nums[0], currMin = nums[0], max = nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            int tempMax = currMax;
+            currMax = Math.max(Math.max(currMax*nums[i], currMin*nums[i]), nums[i]);
+            currMin = Math.min(Math.min(tempMax*nums[i], currMin*nums[i]), nums[i]);
+            max = Math.max(max, currMax);
+        }
+
+        return max;
+    }
+}
+
+// since double negative can make current min current max and one negative can make curr max into curr min
+// therefore we track both of those and use that to calculate max.
+// so at a given point there are three possibilities. 
+// min * val or max * val or val itself. 
