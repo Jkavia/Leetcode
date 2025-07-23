5d18be2557d28eb6be3f72b5486854a0c110598a
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-23 16:42:36 -0700

    Time: 2 ms (11.20%) | Memory: 41.7 MB (6.21%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -2,8 +2,6 @@ class Solution {
     public int reverse(int x) {
         int sign = (x<0)? -1:1;
         String s = String.valueOf(Math.abs(x));
-        //String updated = (x<0)? s.substring(1, s.length()):s;
-        System.out.println(s);
         StringBuffer sb = new StringBuffer();
 
         for(int i=s.length()-1;i>=0;i--){
