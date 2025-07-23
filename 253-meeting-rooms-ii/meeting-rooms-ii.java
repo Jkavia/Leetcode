279f60913f063bd0cfc33ff074b1ceeee06e1f17
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-23 15:31:48 -0700

    Time: 9 ms (35.03%) | Memory: 45.9 MB (89.65%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,18 +1,19 @@
+// we'll use heap to track this based on the end time, peek element is the one getting done fastest
+// now if new element starts at or after the peek element then we remove it and add the new ele
+// constantly keep checking the size of the heap 
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> Integer.compare(a[1],b[1]));
-        int max = 1;
-        for(int[] interval:intervals){
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[1],b[1]));
+        int max =0;
+        for(int[] interval: intervals){
             while(!pq.isEmpty() && pq.peek()[1]<= interval[0]){
                 pq.poll();
             }
             pq.add(interval);
             max = Math.max(max, pq.size());
         }
+
         return max;
     }
 }
-
-// use min-heap to track the rooms that are going to get vacated the earliest 
-// and iterate over sorted list to add/remove rooms based on that 
