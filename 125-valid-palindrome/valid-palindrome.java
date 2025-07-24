62f89a3e23667647bd15c420181a588d734f3fc4
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-24 13:39:40 -0700

    Time: 13 ms (42.76%) | Memory: 45.2 MB (24.19%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String stripped = s.replaceAll("[^a-zA-Z0-9]","");
-        String lowecase = stripped.toLowerCase();
-        int l =0, r = lowecase.length()-1;
-        while(l<=r){
-            if(lowecase.charAt(l) != lowecase.charAt(r)){
-                return false;
-            }
+        String updated = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int l=0,r=updated.length()-1;
+        
+        while(l<r){
+            if(updated.charAt(l)!= updated.charAt(r))return false;
             l++;
             r--;
         }
+
         return true;
     }
 }
