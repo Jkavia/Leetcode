251dbc01d1ae26210a0b286836961f8c5f282637
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-25 15:53:36 -0700

    Time: 38 ms (34.78%) | Memory: 45.2 MB (42.68%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,21 +1,45 @@
+// have a map 
+// iterate thorugh the s, and check for every p elements at a time 
+// if match add
+
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int plen = p.length();
         int slen = s.length();
-        char[] carr = p.toCharArray();
-        Arrays.sort(carr);
-        String sorted = new String(carr);
+        int plen = p.length();
+        System.out.println(slen +"  "+plen);
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c: p.toCharArray()){
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        int count =0;
         List<Integer> ret = new ArrayList<>();
         
-        for(int i=0;i<=slen-plen;i++){
-            String str = s.substring(i, i+plen);
-            char[] arr = str.toCharArray();
-            Arrays.sort(arr);
-            String updated = new String(arr);
-            if(updated.equals(sorted)){
-                ret.add(i);
+        //sliding window 
+        int l=0, r=0;
+
+        while(r<=slen){
+            //System.out.println(count);
+            if(r-l == plen){
+                
+                if(count == map.size()){
+                    ret.add(l);
+                }
+                char c = s.charAt(l);
+                if(map.containsKey(c)){
+                    if(map.get(c) == 0)count--;
+                    map.put(c, map.get(c)+1);
+                }
+                l++;
             }
+            if(r == slen)break;
+            char curr = s.charAt(r);
+            if(map.containsKey(curr)){
+                map.put(curr, map.get(curr)-1);
+                if(map.get(curr) == 0)count++;
+            }
+            r++;
         }
+
         return ret;
     }
 }
