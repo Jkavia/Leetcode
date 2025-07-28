c4e1405d3e7ba95fc049d19b724d64bdef365b44
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-28 15:55:30 -0700

    Time: 35 ms (6.19%) | Memory: 46.1 MB (6.36%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.java
+++ 271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -1,23 +1,24 @@
+// we'll use # as a separator and replace the existing # with ## and do the reverse in decode
 public class Codec {
 
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        StringBuilder sb = new StringBuilder();
+        StringBuffer sb = new StringBuffer();
+
         for(String s: strs){
-            if(s.isEmpty()){sb.append("empty");}else{sb.append(s);}
-            sb.append("₹");
+            sb.append(s.replaceAll("#", "##")).append(" # ");
         }
         return sb.toString();
     }
 
     // Decodes a single string to a list of strings.
-    public List<String> decode(String st) { 
-        //System.out.println(st);
-        String[] strs = st.split("₹");
+    public List<String> decode(String s) {
+        String[] arr = s.split(" # ",-1);
         List<String> ret = new ArrayList<>();
-        
-        for(String s:strs){
-            if(s.equals("empty")){ret.add("");}else{ret.add(s);}
+
+        for(int i =0;i<arr.length-1;i++){
+            String str= arr[i];
+            ret.add(str.replaceAll("##","#"));
         }
         return ret;
     }
