ebcf78b7e8b65e311e442031ca8d623159c4aa0d
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-30 14:29:24 -0700

    Time: 1 ms (100.00%) | Memory: 44 MB (99.64%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,25 +1,30 @@
+//similar to mark the islands with the id.
+// do union find
 class Solution {
     public int countComponents(int n, int[][] edges) {
-        int[] roots = new int[n];
-        for(int i=0;i<n;i++)roots[i] = i;
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)arr[i] = i;
 
         for(int[] edge: edges){
-        int root1 = find(roots, edge[0]);
-        int root2 = find(roots, edge[1]);
-        if(root1 != root2){
-            roots[root1] = root2;
-            n--;
-        }
+            int root1 = find(arr, edge[0]);
+            int root2 = find(arr, edge[1]);
+            // if they're already not marked as same island we need to mark it as same island.
+            if(root1 != root2){
+                arr[root1] = root2;
+                n--;
+            }
         }
         return n;
     }
 
 
-    public int find(int[] roots, int id){
+    public int find(int[] arr, int id){
 
-        while(roots[id]!= id){
-            roots[id] = roots[roots[id]];
-            id = roots[id];
+        while(arr[id] != id){
+            //do this to get to the parent node as when we reach parent thats when arr[id] == id
+            // since in the code above we'd already set that  arr[root1] = root2;
+            arr[id] = arr[arr[id]];
+            id = arr[id];
         }
         return id;
     }
