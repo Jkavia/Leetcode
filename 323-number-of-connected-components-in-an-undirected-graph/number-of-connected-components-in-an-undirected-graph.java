2c3f8700b295057c73f87fd9594fdf8c17e2ccd9
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-07-29 19:08:57 -0700

    Time: 1 ms (100.00%) | Memory: 44.6 MB (71.54%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,35 +1,26 @@
 class Solution {
-    // just count the nodes with indegree 0
     public int countComponents(int n, int[][] edges) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for(int i=0;i<n;i++){
-            map.put(i, new ArrayList<>());
+        int[] roots = new int[n];
+        for(int i=0;i<n;i++)roots[i] = i;
+
+        for(int[] edge: edges){
+        int root1 = find(roots, edge[0]);
+        int root2 = find(roots, edge[1]);
+        if(root1 != root2){
+            roots[root1] = root2;
+            n--;
         }
-
-        for(int[] edge:edges){
-            map.get(edge[0]).add(edge[1]);
-            map.get(edge[1]).add(edge[0]);
         }
-        int count =0;
-        Set<Integer> visited = new HashSet<>();
-
-        for(int i=0;i<n;i++){
-
-            if(!visited.contains(i)){
-                count++;
-                dfs(map, i, visited);
-                }
-        }
-
-        return count;
+        return n;
     }
 
 
-    public void dfs(Map<Integer, List<Integer>> map, int node, Set<Integer> visited){
-        if(visited.contains(node))return;
-        visited.add(node);
-        for(int n: map.get(node)){
-            dfs(map, n, visited);
+    public int find(int[] roots, int id){
+
+        while(roots[id]!= id){
+            roots[id] = roots[roots[id]];
+            id = roots[id];
         }
+        return id;
     }
 }
