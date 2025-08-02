71d5a6c72fccd89a90e52c6eaea996bccb313e9b
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-08-02 15:07:09 -0700

    Time: 5 ms (55.03%) | Memory: 45.7 MB (58.74%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,51 +1,56 @@
-// left and right side dfs 
-// top and bottom dfs 
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int m = heights.length;
-        int n = heights[0].length;
+        int row = heights.length;
+        int col = heights[0].length;
 
-        boolean[][] visistedPacific = new boolean[m][n];
-        boolean[][] visistedAtlantic = new boolean[m][n];
+        boolean[][] pacific = new boolean[row][col];
+        boolean[][] atlantic = new boolean[row][col];
 
-        // left and right edges dfs
-        for(int i=0;i<m;i++){
-            dfs(i,0,visistedPacific, heights);
-            dfs(i, n-1,visistedAtlantic, heights);
+        // left and right
+        for(int i=0;i<row;i++){
+            dfs(i, 0, pacific,heights,row,col);
+            dfs(i, col-1, atlantic,heights,row,col);
         }
 
-        // top and bottom edges dfs
-        for(int j=0;j<n;j++){
-            dfs(0,j,visistedPacific, heights);
-            dfs(m-1,j,visistedAtlantic, heights);
+        // up and down
+        for(int j=0;j<col;j++){
+            dfs(0, j, pacific,heights,row,col);
+            dfs(row-1, j, atlantic,heights,row,col);
         }
+
         List<List<Integer>> ret = new ArrayList<>();
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(visistedPacific[i][j] && visistedAtlantic[i][j]){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(atlantic[i][j] && pacific[i][j]){
                     ret.add(Arrays.asList(i,j));
                 }
             }
         }
 
-        return ret; 
+        return ret;
     }
 
-    public void dfs(int x, int y, boolean[][] visited, int[][] heights){
-        int m = visited.length;
-        int n = visited[0].length;
-        visited[x][y] = true;
 
-        int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
+    public void dfs(int i, int j, boolean[][] visited, int[][] heights, int row, int col){
+        visited[i][j] = true;
+        int[][] dirs = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
 
         for(int[] dir: dirs){
-            int i = x+dir[0];
-            int j = y+dir[1];
+            int x = i+dir[0];
+            int y = j+dir[1];
 
-            if(i>=0 && i<m && j>=0 && j<n && !visited[i][j] && heights[x][y]<=heights[i][j]){
-                dfs(i,j,visited, heights);
+            if(x>=0 && x<row && y>=0 && y<col && !visited[x][y] && heights[i][j] <= heights[x][y]){
+                dfs(x,y,visited,heights,row,col);
             }
         }
-        }
+    }
 }
+
+// so we'll do dfs from top and bottom for pac and atla
+// and then we'll do the same for left and right 
+// so as we know that water can fall from higher plane to lower or same hegiht plane. 
+// we'll look for the neighbors close to border cells that are higher or equal to that cell 
+// that would tell us if the water can fall into that cell and then ventually into the ocean
+// then we'll repeat the process for next neighboring cells in dfs fashion while maintiaing a visited 
+// here we can see both visited 
