d8bea741a0be4a53015431cbdbd3b4fd05f41df1
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-08-03 18:06:46 -0700

    Time: 1 ms (100.00%) | Memory: 44.6 MB (79.83%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,36 +1,29 @@
 class Solution {
     public int countComponents(int n, int[][] edges) {
-        Set<Integer> visited = new HashSet<>();
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for(int i=0;i<n;i++)map.put(i, new ArrayList<>());
-        for(int[] edge: edges){
-            map.get(edge[0]).add(edge[1]);
-            map.get(edge[1]).add(edge[0]);
-        }
-        int count =0;
-        for(int i=0;i<n;i++){
-            if(!visited.contains(i)){
-                dfs(i, map, visited);
-                count++;
+        int[] ids = new int[n];
+        for(int i=0;i<n;i++)ids[i] =i;
+
+        for(int[] edge:edges){
+            int root1 = union(edge[0],ids);
+            int root2 = union(edge[1], ids);
+
+            if(root1 != root2){
+                ids[root1] = root2;
+                n--;
             }
         }
-
-        return count;
+        return n;
     }
 
+    public int union(int id, int[] arr){
 
-    public void dfs(int node, Map<Integer, List<Integer>> map, Set<Integer> visited){
-        if(visited.contains(node))return;
-
-        visited.add(node);
-
-        for(int currNode: map.get(node)){
-            dfs(currNode, map, visited);
+        while(arr[id] != id){
+            arr[id] = arr[arr[id]];
+            id = arr[id];
         }
+        return id;
     }
 }
 
-// check if there are enough edges i.e for n nodes n-1 edges should be there. 
-// then maintain a visited array and run it for each element if its not visited 
-// run a dfs add all to visited and increase the count of edge 
-// run it for rest of the nodes and increment conected count. 
+// can be solved using union find also 
+// where we mark the visited node with the parents id 
