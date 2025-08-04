1e5fd543fbf6ef4c1e234b0a24e0eb9800f88796
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-08-03 17:52:00 -0700

    Time: 7 ms (32.47%) | Memory: 46.2 MB (6.98%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,31 +1,36 @@
-//similar to mark the islands with the id.
-// do union find
 class Solution {
     public int countComponents(int n, int[][] edges) {
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++)arr[i] = i;
-
+        Set<Integer> visited = new HashSet<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int i=0;i<n;i++)map.put(i, new ArrayList<>());
         for(int[] edge: edges){
-            int root1 = find(arr, edge[0]);
-            int root2 = find(arr, edge[1]);
-            // if they're already not marked as same island we need to mark it as same island.
-            if(root1 != root2){
-                arr[root1] = root2;
-                n--;
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
+        }
+        int count =0;
+        for(int i=0;i<n;i++){
+            if(!visited.contains(i)){
+                dfs(i, map, visited);
+                count++;
             }
         }
-        return n;
+
+        return count;
     }
 
 
-    public int find(int[] arr, int id){
+    public void dfs(int node, Map<Integer, List<Integer>> map, Set<Integer> visited){
+        if(visited.contains(node))return;
 
-        while(arr[id] != id){
-            //do this to get to the parent node as when we reach parent thats when arr[id] == id
-            // since in the code above we'd already set that  arr[root1] = root2;
-            arr[id] = arr[arr[id]];
-            id = arr[id];
+        visited.add(node);
+
+        for(int currNode: map.get(node)){
+            dfs(currNode, map, visited);
         }
-        return id;
     }
 }
+
+// check if there are enough edges i.e for n nodes n-1 edges should be there. 
+// then maintain a visited array and run it for each element if its not visited 
+// run a dfs add all to visited and increase the count of edge 
+// run it for rest of the nodes and increment conected count. 
