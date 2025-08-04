71a3dd8977dd4c2f8527c03b6d82df8fbac0a9c5
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-08-03 19:28:27 -0700

    Time: 9 ms (34.52%) | Memory: 46.1 MB (66.52%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,13 +1,11 @@
-// we'll use heap to track this based on the end time, peek element is the one getting done fastest
-// now if new element starts at or after the peek element then we remove it and add the new ele
-// constantly keep checking the size of the heap 
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[1],b[1]));
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+
         int max =0;
         for(int[] interval: intervals){
-            while(!pq.isEmpty() && pq.peek()[1]<= interval[0]){
+            if(!pq.isEmpty() && interval[0] >= pq.peek()[1]){
                 pq.poll();
             }
             pq.add(interval);
@@ -17,3 +15,8 @@ class Solution {
         return max;
     }
 }
+
+// maintain a min heap sorted by the end time 
+// if you find an interval whose start time is after lowest end time 
+// remove the top element and add this 
+// keep a running track of heap size 
