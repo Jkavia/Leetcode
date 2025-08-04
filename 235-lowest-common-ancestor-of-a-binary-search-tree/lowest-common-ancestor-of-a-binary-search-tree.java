87afb54ec3948335db0cf4ca2f6d372c9d84fb07
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-08-03 20:16:17 -0700

    Time: 6 ms (28.23%) | Memory: 44.9 MB (91.64%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,11 +9,10 @@
  */
 
 class Solution {
-    TreeNode lca = null;
-
+    TreeNode parent = null;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         lcaHelper(root, p, q);
-        return lca;
+        return parent;
     }
 
     public void lcaHelper(TreeNode root, TreeNode p, TreeNode q){
@@ -21,11 +20,15 @@ class Solution {
 
         if(p.val < root.val && q.val < root.val){
             lcaHelper(root.left, p, q);
-        }else if (p.val > root.val && q.val > root.val){
+        }else if(p.val > root.val && q.val > root.val){
             lcaHelper(root.right, p, q);
         }else{
-            lca = root;
-            return;
+            parent = root;
         }
     }
 }
+
+// if both nodes are smaller than root then we move left 
+// if both nodes are greater than root then we move to the right 
+// now if one is greater than equal and one is less than equal 
+// then that means its the common ancestor !! 
