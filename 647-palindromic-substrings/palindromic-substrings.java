39883bd8b5decc2b0affb1828892333c81b89066
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-08-05 18:29:17 -0700

    Time: 5 ms (97.03%) | Memory: 41.3 MB (93.22%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,53 +1,28 @@
-// we can check plaindrome for length 2 to length of string
-// but in the process we might end up recomputing some of the other 
-// previously done computing therefore lets just just store that info into 
-// 2d dp array which tracks if the string between ij is palin or not. 
-
 class Solution {
     public int countSubstrings(String s) {
-        int len = s.length();
-        boolean[][] palin = new boolean[len][len];
+        int ret =0;
 
-        for(int i=0;i<len;i++)palin[i][i] = true;
-
-        for(int i=1;i<len;i++){
-            for(int j=0;j<len;j++){
-                if(j+i < len){
-                    if(!palin[j][i+j] && ifPalin(s, j, j+i, palin)){
-                        palin[j][i+j] = true;
-                    }
-                }
-            }
+        for(int i=0;i<s.length();i++){
+            ret+= ispalin(s, i, i);
+            ret+= ispalin(s, i, i+1);
         }
 
+        return ret;
+    }
+
+    public int ispalin(String s, int i, int j){
         int count =0;
 
-        for(int i=0;i<len;i++){
-            for(int j=0;j<len;j++){
-                if(palin[i][j]){
-                    count++;
-                }
-            }
+        while(i>=0 && j<s.length() && s.charAt(i) == s.charAt(j)){
+            i--;
+            j++;
+            count++;
         }
 
         return count;
-
-    }
-
-    public boolean ifPalin(String s, int i, int j, boolean[][] palin){
-        //System.out.println(i+" <> "+j);
-        while(i<j){
-            if(palin[i][j]){
-                //System.out.println("true");
-                return true;
-            }else if(s.charAt(i) != s.charAt(j)){
-                //System.out.println("false");
-                return false;
-            }
-            i++;
-            j--;
-        }
-        //System.out.println("true");
-        return true;
     }
 }
+
+// instead of outside in char comparisons 
+// we do inside out and there are only two possiblities either odd length 
+// and even length strings, 
