968fd54830499c1408a6f66ca4c8c53d5c627ec7
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-08-11 17:45:57 -0700

    Time: 0 ms (100.00%) | Memory: 44.2 MB (77.88%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -15,30 +15,24 @@
  */
 class Solution {
     int maxSum = Integer.MIN_VALUE;
+
     public int maxPathSum(TreeNode root) {
-        pathSum(root);
+        maxPathHelper(root);
 
         return maxSum;
     }
 
-    public int pathSum(TreeNode root){
+    public int maxPathHelper(TreeNode root){
         if(root == null)return 0;
 
-        int rootVal = root.val;
+        int left = maxPathHelper(root.left);
+        int right = maxPathHelper(root.right);
+        int leftVal = left < 0? 0: left;
+        int rightVal = right < 0? 0: right;
 
-        int left = pathSum(root.left);
-        int right = pathSum(root.right);
+        maxSum = Math.max(maxSum, leftVal+rightVal+root.val);
 
-        int adjustedLeft = (left<0)?0:left;
-        int adjustedRight = (right<0)?0:right;
-        maxSum = Math.max(maxSum, rootVal+adjustedLeft+adjustedRight);
-        return rootVal+Math.max(adjustedLeft, adjustedRight);
+        return Math.max(leftVal+root.val, rightVal+root.val);
+
     }
 }
-
-//here we need to maintain a class level variable that tracks max at each node level
-// we do the dfs on both left and right side and on its way back we add the node values 
-// but if a left or right node value is negative then we consider it as good as none so we default it to 0 
-// and then at each root node level we just add the max of left or right to root value and send it. 
-// what this is doing is if at that root it was a max sum it would be registed in class level variable 
-// and it it wasnt then the best we can do from that level is to send back the max sum from either of the paths or none if negative. 
