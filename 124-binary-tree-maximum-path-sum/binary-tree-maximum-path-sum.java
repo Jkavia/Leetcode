e1800df5cbfa77a073cb96c5f03cb046bd403506
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-08-13 15:27:02 -0700

    Time: 0 ms (100.00%) | Memory: 45 MB (8.36%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -14,25 +14,20 @@
  * }
  */
 class Solution {
-    int maxSum = Integer.MIN_VALUE;
-
+    int maxSum=Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        maxPathHelper(root);
-
+        maxPathSumHelper(root);
         return maxSum;
     }
 
-    public int maxPathHelper(TreeNode root){
+    public int maxPathSumHelper(TreeNode root){
         if(root == null)return 0;
 
-        int left = maxPathHelper(root.left);
-        int right = maxPathHelper(root.right);
-        int leftVal = left < 0? 0: left;
-        int rightVal = right < 0? 0: right;
-
+        int left = maxPathSumHelper(root.left);
+        int right = maxPathSumHelper(root.right);
+        int leftVal = left < 0 ? 0:left;
+        int rightVal = right < 0 ? 0:right;
         maxSum = Math.max(maxSum, leftVal+rightVal+root.val);
-
         return Math.max(leftVal+root.val, rightVal+root.val);
-
     }
 }
