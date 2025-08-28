82be7dc8e1d8009e70fcbf5ca46d2b1b7d95f4e9
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-08-27 21:04:21 -0700

    Time: 0 ms (100.00%) | Memory: 41.9 MB (98.47%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,31 +1,44 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        
-        int len = matrix.length;
+        int rowlen  = matrix.length;
         int collen = matrix[0].length;
 
-        for(int row =0;row <= len/2;row++){
-            for(int col = row; col < collen-1-row; col++){
+        for(int row =0;row<rowlen/2;row++){
+            for(int col =row;col<collen-1-row;col++){
                 int val = matrix[row][col];
                 int count =0;
+
                 while(count < 4){
                     int nrow = col;
                     int ncol = collen-1-row;
                     int temp = matrix[nrow][ncol];
                     matrix[nrow][ncol] = val;
                     val = temp;
-                    col = ncol;
                     row = nrow;
+                    col = ncol;
                     count++;
                 }
             }
         }
-
     }
 }
+/** 
+when you rotate the 
+the row of the next postion becomes col of previous one  
+and row becomes len -1-row
 
-// row = col 
-// col = len -1-row
+00 > 02
 
-// 00 > 02 row = col = 0 & col = 2-0 = 2
-// 02 > 22 row = col = 2 & col = 2-0 = 2
+col = row(prev) 0
+row = len-1-col(prev) 3-1-0
+so 02
+
+similary 
+02 > 22
+row1 = col0 = 2
+col1 = len-1-row0 = 3-1-0 =2
+22
+
+now we need to make sure that we rotate only half of the rows 
+and concetric metrix so col start with row no and goes upto len-1-row
+**/
