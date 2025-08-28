5afeaaaff55fd376d5f397f711422a1ba87d99ad
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-08-27 20:23:45 -0700

    Time: 10 ms (73.91%) | Memory: 46.4 MB (21.45%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -8,44 +8,47 @@
  * }
  */
 public class Codec {
+   String separator = ",";
+   String nullVal = "null";
 
-    // Encodes a tree to a single string. Use stringbuffer and recursion 
+    // Encodes a tree to a single string. using the left first traversal append all in stringbuffer
     public String serialize(TreeNode root) {
         StringBuffer sb = new StringBuffer();
-        String nullVal = "null";
-        String separator = ",";
-        serializeHelper(root, sb, nullVal, separator);
+
+        serializeHelper(root, sb);
         return sb.toString();
     }
 
-    public void serializeHelper(TreeNode root, StringBuffer sb, String nullVal, String separator){
+    public void serializeHelper(TreeNode root, StringBuffer sb){
         if(root == null){
             sb.append(nullVal).append(separator);
             return;
         }
 
         sb.append(root.val).append(separator);
-        serializeHelper(root.left, sb, nullVal, separator);
-        serializeHelper(root.right, sb, nullVal, separator);
+        serializeHelper(root.left, sb);
+        serializeHelper(root.right, sb);
     }
 
-    // Decodes your encoded data to tree. Use que and recursion 
+    // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        String[] vals = data.split(separator);
         Queue<String> que = new LinkedList<>();
-        que.addAll(Arrays.asList(data.split(",")));
-        
+
+        que.addAll(Arrays.asList(vals));
+
         return deserializeHelper(que);
     }
 
     public TreeNode deserializeHelper(Queue<String> que){
         if(que.isEmpty())return null;
         String val = que.poll();
-
-        if(val.equals("null")){
+        if(val.equals(nullVal)){
             return null;
         }
 
         TreeNode root = new TreeNode(Integer.parseInt(val));
+
         root.left = deserializeHelper(que);
         root.right = deserializeHelper(que);
 
