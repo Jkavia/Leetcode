96a155135ba6c8989f6f9f3f2cdae2f7b30c5f53
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-09-15 17:33:10 -0700

    Time: 2 ms (98.89%) | Memory: 45.3 MB (10.33%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //since its not sorted so we use map, cause sorting it will lose the index
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
-            int num = nums[i];
-            int lookup = target-num;
-            if(map.containsKey(lookup)){
-                return new int[]{map.get(lookup), i};
+            int val = target - nums[i];
+            if(map.containsKey(val)){
+                return new int[]{map.get(val), i};
             }
-            map.put(num, i);
+            map.put(nums[i],i);
         }
 
         return new int[2];
