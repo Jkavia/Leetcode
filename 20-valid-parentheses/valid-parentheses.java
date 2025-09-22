68eea96412833b33f9c9c65601331a18aec03a2f
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-09-21 17:30:31 -0700

    Time: 3 ms (47.82%) | Memory: 42.2 MB (18.22%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
         Map<Character, Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+
         Stack<Character> stck = new Stack<>();
-        map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
 
         for(char c: s.toCharArray()){
-            if(map.containsKey(c)){
-                if(stck.isEmpty() || stck.peek() != map.get(c)){
-                    return false;
-                }
+            if(map.containsKey(c) && !stck.isEmpty()){
+                if(stck.peek() != map.get(c)) return false;
                 stck.pop();
             }else{
                 stck.push(c);
             }
-
         }
 
-        return stck.isEmpty() && true;
-
+        return stck.isEmpty();
     }
 }
