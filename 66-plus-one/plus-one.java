8c25f1baf6325d1bb258502856a8c66c124b5c2d
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-09-21 17:57:29 -0700

    Time: 0 ms (100.00%) | Memory: 42 MB (31.69%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,26 +1,29 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int carry =1;
-        Stack<Integer> stck = new Stack<>();
+        int count =0;
+        for(int digit:digits){
+            if(digit == 9)count++;
+        }
+        int[] ret;
+        if(count == digits.length){
+            ret = new int[digits.length+1];
+        }else{
+            ret = new int[digits.length];
+        }
 
         for(int i=digits.length-1;i>=0;i--){
             int currTotal = digits[i]+carry;
             int numToAdd = currTotal%10;
             //System.out.println(numToAdd);
-            stck.push(numToAdd);
+            ret[i] = numToAdd;
             if(currTotal>=10){
                 carry =1;
             }else{
                 carry=0;
             }
         }
-        if(carry == 1){stck.push(1);}
-        int[] ret = new int[stck.size()];
-        int idx =0;
-        while(!stck.isEmpty()){
-            int val = stck.pop();
-            ret[idx++] = val;
-        }
+        if(carry == 1){ret[0] =1;}
 
         return ret;
     }
