323d267869ecd538e8d93af4f586c38996c38530
Janardan Singh Kavia <janardankavia91@gmail.com>
2025-09-21 18:02:46 -0700

    Time: 0 ms (100.00%) | Memory: 40.7 MB (28.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,14 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<3)return n;
-        int[] arr = new int[n+1];
-        arr[1]=1;
-        arr[2]=2;
-        for(int i=3;i<arr.length;i++){
-            arr[i] = arr[i-1]+arr[i-2];
+        if(n<=2)return n;
+
+        int prev = 2;
+        int prePrev = 1;
+        int total =0;
+
+        for(int i=3;i<=n;i++){
+            total = prev+prePrev;
+            prePrev = prev;
+            prev = total;
         }
-        return arr[n];
+        return total;
     }
 }
-
-// no of ways to reach the current spot = (no of ways to reach n-1)+(no of ways to reach n-2)
